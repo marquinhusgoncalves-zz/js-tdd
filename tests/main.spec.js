2a6c8f9f711b855877bcab4855c6d41b3c821e56
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-01-21 21:55:54 -0200

    76 - Funcionamento do Mocha - describe, context, it

---
+++ tests/main.spec.js
@@ -0,0 +1,23 @@
+describe('Main', () => {
+  describe('Method A', () => {
+    context('Case 1', () => {
+      it('should happen blabla', () => {
+
+      });
+    });
+
+    context('Case 2', () => {
+      it('should happen blabla', () => {
+
+      });
+
+      it('should happen mimimi', () => {
+
+      });
+    });
+  });
+
+  describe('Method B', () => {
+
+  });
+});
