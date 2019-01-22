703fc3cb78c3d61039090f98416671fab2b119d6
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-01-21 22:16:07 -0200

    79 - Introdução aos Hooks do Mocha

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,23 +1,59 @@
+// describe('Main', () => {
+//   describe('Method A', () => {
+//     context('Case 1', () => {
+//       it('should happen blabla', () => {
+
+//       });
+//     });
+
+//     context('Case 2', () => {
+//       it('should happen blabla', () => {
+
+//       });
+
+//       it('should happen mimimi', () => {
+
+//       });
+//     });
+//   });
+
+//   describe('Method B', () => {
+
+//   });
+// });
+
 describe('Main', () => {
-  describe('Method A', () => {
-    context('Case 1', () => {
-      it('should happen blabla', () => {
+  let arr = [];
 
-      });
-    });
+  // roda uma vez, antes do bloco
+  before(() => {
 
-    context('Case 2', () => {
-      it('should happen blabla', () => {
-
-      });
-
-      it('should happen mimimi', () => {
-
-      });
-    });
   });
 
-  describe('Method B', () => {
+  after(() => {
 
+  });
+
+  beforeEach(() => {
+    arr = [1, 2, 3];
+  });
+
+  afterEach(() => {
+
+  });
+
+  it('should have a size of 4 when push another value to the array', () => {
+    arr.push(4);
+    console.log(arr.length); // 4
+  });
+
+  it('should have a size of 2 when pop a value from the array', () => {
+    arr.pop();
+    console.log(arr.length); // 2
+  });
+
+  it('should remove the value 3 when use pop in the array', () => {
+    arr.pop();
+    console.log(arr.pop() === 3); // true
   });
 });
