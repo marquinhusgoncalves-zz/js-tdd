be2e5f5a3484c9c1c0b4500fac86520b14aa0dbc
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-02-05 21:17:49 -0200

    90 - Criando smoke tests

---
+++ spotify-wrapper/src/main.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+import { search, searchAlbuns, searchArtists, searchTracks, searchPlaylists } from '../src/main';
+
+describe('Spotify Wrapper', () => {
+  describe('smoke tests', () => {
+    it('should exists the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exists the searchAlbuns method', () => {
+      expect(searchAlbuns).to.exist;
+    });
+
+    it('should exists the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('should exists the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exists the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
+});
---
+++ spotify-wrapper/tests/main.spec.js
@@ -0,0 +1,9 @@
+export const search = () => { };
+
+export const searchAlbuns = () => { };
+
+export const searchArtists = () => { };
+
+export const searchTracks = () => { };
+
+export const searchPlaylists = () => { };
