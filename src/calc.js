08e2b17da73710ccc3351d7b82ce812f99cb5d41
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-02-04 22:41:43 -0200

    83 - Criando uma calculadora simples

--- src/calc.js
+++ src/calc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  sum: () => {},
-  sub: () => {},
-  div: () => {},
-  mult: () => {},
+  sum: (num1, num2) => num1 + num2,
+  sub: (num1, num2) => num1 - num2,
+  mult: (num1, num2) => num1 * num2,
+  div: (num1, num2) => (num2 === 0 ? 'Não  é possível divisão por 0!' : num1 / num2),
 };
