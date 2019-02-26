250276853408eb83e5d8579a761f5ccf5770f75f
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-02-26 20:52:38 -0300

    102 - Criando um exemplo simples com a bliblioteca UMD

--- spotify-wrapper/lib/search.js
+++ spotify-wrapper/lib/search.js
@@ -7,8 +7,6 @@ exports.searchPlaylists = exports.searchTracks = exports.searchAlbums = exports.
 
 var _config = require('./config');
 
-var _config2 = _interopRequireDefault(_config);
-
 var _utils = require('./utils');
 
 var _utils2 = _interopRequireDefault(_utils);
@@ -16,23 +14,22 @@ var _utils2 = _interopRequireDefault(_utils);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /* global fetch */
-
 var search = exports.search = function search(query, type) {
-  return fetch(_config2.default + '/search?q=' + query + '&type=' + type).then(_utils2.default);
+  return fetch(_config.API_URL + '/search?q=' + query + '&type=' + type).then(_utils2.default);
 };
 
 var searchArtists = exports.searchArtists = function searchArtists(query) {
-  search(query, 'artist');
+  return search(query, 'artist');
 };
 
 var searchAlbums = exports.searchAlbums = function searchAlbums(query) {
-  search(query, 'album');
+  return search(query, 'album');
 };
 
 var searchTracks = exports.searchTracks = function searchTracks(query) {
-  search(query, 'track');
+  return search(query, 'track');
 };
 
 var searchPlaylists = exports.searchPlaylists = function searchPlaylists(query) {
-  search(query, 'playlist');
+  return search(query, 'playlist');
 };
--- spotify-wrapper/src/search.js
+++ spotify-wrapper/src/search.js
@@ -1,23 +1,18 @@
 /* global fetch */
-
-import API_URL from './config';
+import { API_URL } from './config';
 import toJSON from './utils';
 
-export const search = (query, type) => fetch(`${API_URL}/search?q=${query}&type=${type}`)
-  .then(toJSON);
+export const search = (query, type) =>
+  fetch(`${API_URL}/search?q=${query}&type=${type}`).then(toJSON);
 
-export const searchArtists = (query) => {
+export const searchArtists = query =>
   search(query, 'artist');
-};
 
-export const searchAlbums = (query) => {
+export const searchAlbums = query =>
   search(query, 'album');
-};
 
-export const searchTracks = (query) => {
+export const searchTracks = query =>
   search(query, 'track');
-};
 
-export const searchPlaylists = (query) => {
+export const searchPlaylists = query =>
   search(query, 'playlist');
-};
