bd87789beb8324b8fb04a9699d5c64b2fb81a24c
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-03-10 19:53:50 -0300

    120 - Criando o component AlbumInfo

---
+++ spotify-wrapper-player/src/AlbumInfo.js
@@ -0,0 +1,12 @@
+export default function renderAlbumInfo(data, element) {
+  const markup = `
+    <img class="album-image" src="${data.images[0].url}" alt="${data.name}">
+    <p class="album-title">${data.name}</p>
+    <p class="album-artist">${data.artists[0].name}</p>
+    <p class="album-counter">${data.tracks.total} Músicas</p>
+  `;
+
+  /* eslint-disable no-param-reassign */
+  element.innerHTML = markup;
+  /* eslint-enable no-param-reassign */
+}
