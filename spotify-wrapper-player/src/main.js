f488af6bbcccd5b175e7834f680976d87ecdb25b
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-03-10 22:47:22 -0300

    128 - Criando PlaylistTrigger

--- spotify-wrapper-player/src/main.js
+++ spotify-wrapper-player/src/main.js
@@ -1,7 +1,9 @@
 import searchEnterTrigger from './SearchTrigger';
 import selectAlbumTrigger from './SearchAlbumTrigger';
-
+import playlistTrigger from './PlaylistTrigger';
 
 searchEnterTrigger();
 
 selectAlbumTrigger();
+
+playlistTrigger();
