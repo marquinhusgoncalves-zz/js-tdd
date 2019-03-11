7a9bbf4d9f01fd28d29d05205836986d41e6c597
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-03-11 20:13:39 -0300

    132 - Conhecendo o Commander

--- btc-converter/tests/main.spec.js
+++ btc-converter/tests/main.spec.js
@@ -2,12 +2,21 @@ const expect = require('chai').expect;
 
 const exec = require('child_process').exec;
 const btcConverter = './src/main.js';
+const pkg = require('../package.json');
 
 describe('Main CLI', () => {
-  it('should return Hello World', (done) => {
-    exec(btcConverter, (err, stdout, stderr) => {
+  it('should return version of btc-converter', (done) => {
+    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
       if (err) throw err;
-      expect(stdout.replace('\n', '')).to.be.equal('Hello World!');
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+      done();
+    });
+  });
+
+  it('should return the when description btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.includes('Convert Bitcoin to any currency defined')).to.be.true;
       done();
     });
   });
