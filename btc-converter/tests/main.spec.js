a7af39b7dea2845c5aa6150cc406bd818a73638b
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-03-11 20:03:21 -0300

    131 - Iniciando a CLI e testes

--- btc-converter/tests/main.spec.js
+++ btc-converter/tests/main.spec.js
@@ -1,5 +1,14 @@
-import { expect } from 'chai';
+const expect = require('chai').expect;
 
-describe('Main', () => {
+const exec = require('child_process').exec;
+const btcConverter = './src/main.js';
 
+describe('Main CLI', () => {
+  it('should return Hello World', (done) => {
+    exec(btcConverter, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal('Hello World!');
+      done();
+    });
+  });
 });
