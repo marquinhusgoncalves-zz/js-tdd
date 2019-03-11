827bab693c9b70a8af01802535b932010376df7b
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-03-10 21:08:16 -0300

    123 - Adicionando AlbumTracks no layout

--- spotify-wrapper-player/src/AlbumInfo.js
+++ spotify-wrapper-player/src/AlbumInfo.js
@@ -1,12 +1,18 @@
-export default function renderAlbumInfo(data, element) {
-  const markup = `
+function renderMarkup(data) {
+  return (`
     <img class="album-image" src="${data.images[0].url}" alt="${data.name}">
     <p class="album-title">${data.name}</p>
     <p class="album-artist">${data.artists[0].name}</p>
     <p class="album-counter">${data.tracks.total} Músicas</p>
-  `;
+  `);
+}
+
+export default function renderAlbumInfo(data, element) {
+  const markup = renderMarkup(data);
 
   /* eslint-disable no-param-reassign */
   element.innerHTML = markup;
   /* eslint-enable no-param-reassign */
+
+  return data;
 }
--- spotify-wrapper-player/src/main.js
+++ spotify-wrapper-player/src/main.js
@@ -3,15 +3,18 @@
 import spotify from './Spotify';
 import rendeAlbums from './AlbumList';
 import renderAlbumInfo from './AlbumInfo';
+import renderAlbumTracks from './AlbumTracks';
 
 const albums = spotify.search.albums('Incubus');
 const albumList = document.getElementById('album-list');
 
 const album = spotify.album.getAlbum('6peEdPVO73WtgGah5sEhX4');
 const albumInfo = document.getElementById('album-info');
+const albumTracks = document.getElementById('album-tracks');
 
 albums
   .then(data => rendeAlbums(data.albums.items, albumList));
 
 album
-  .then(data => renderAlbumInfo(data, albumInfo));
+  .then(data => renderAlbumInfo(data, albumInfo))
+  .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
