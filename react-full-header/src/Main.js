bd19b1f8bd0aec5bf740dd9b212be78599123c1e
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-03-11 23:02:26 -0300

    143 - Escrevendo stories

--- react-full-header/src/Main.js
+++ react-full-header/src/Main.js
@@ -1,12 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Component = ({ name }) => (
+const FullHeader = ({ name }) => (
     <h1>Hello {name}!</h1>
 );
 
-Component.propTypes = {
-    name: PropTypes.string.isRequired,
+FullHeader.propTypes = {
+  name: PropTypes.string.isRequired,
 };
 
-export default Component;
+export default FullHeader;
--- react-full-header/stories/Main.js
+++ react-full-header/stories/Main.js
@@ -1,11 +1,57 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-import Component from '../src/Main'; // This is our component
+import FullHeader from '../src/Main'; // This is our component
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Component', module)
-    .add('with lyef name', () => (
-        <Component name="lyef" />
-    ))
-    .add('with another name', () => (
-        <Component name="another" />
-    ))
+storiesOf('FullHeader', module)
+  .add('with title', () => (
+    <FullHeader title="TDD" />
+  ))
+  .add('with title and subtitle', () => (
+    <FullHeader title="TDD" subtitle="Curso de JS com TDD na prática" />
+  ))
+  .add('with title and subtitle', () => (
+    <FullHeader
+      title="TDD"
+      subtitle="Curso de JS com TDD na prática" />
+  ))
+  .add('with title, subtitle, bgColor', () => (
+    <FullHeader
+      title="TDD"
+      subtitle="Curso de JS com TDD na prática"
+      bgColor="#3299BB"
+    />
+  ))
+  .add('with title, subtitle, bgColor, textColor', () => (
+    <FullHeader
+      title="TDD"
+      subtitle="Curso de JS com TDD na prática"
+      bgColor="#3299BB"
+      textColor="#FF9900"
+    />
+  ))
+  .add('with title, subtitle, bgColor, textColor, font', () => (
+    <FullHeader
+      title="TDD"
+      subtitle="Curso de JS com TDD na prática"
+      bgColor="#3299BB"
+      textColor="#FF9900"
+      font="cursive"
+    />
+  ))
+  .add('with title, subtitle, bgImage', () => (
+    <FullHeader
+      title="TDD"
+      subtitle="Curso de JS com TDD na prática"
+      bgImg="https://raw.githubusercontent.com/willianjusten/photo-examples/master/iceland-glacier.jpg"
+    />
+  ))
+  .add('with title, subtitle, video', () => (
+    <FullHeader
+      title="TDD"
+      subtitle="Curso de JS com TDD na prática"
+      bgColor="#3299BB"
+      textColor="#FF9900"
+      video="http://callmenick.com/_development/video/full-screen-background-video/media/demo.mp4"
+    />
+  ));
