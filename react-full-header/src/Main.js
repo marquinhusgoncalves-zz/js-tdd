490de23cccb945ca260286fa89bbf549f41a080c
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-03-12 01:17:58 -0300

    151 - Adicionando vídeo

--- react-full-header/src/Main.js
+++ react-full-header/src/Main.js
@@ -1,9 +1,26 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { defaultProps, headerSyle, containerStyle, titleStyle, subTitleStyle } from './Styles';
+import {
+    defaultProps,
+    headerSyle,
+    containerStyle,
+    titleStyle,
+    subTitleStyle,
+    videoStyle,
+} from './Styles';
 
-const FullHeader = ({ title, subtitle, bgColor, textColor, font, bgImg }) => {
+const propTypes = {
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    bgColor: PropTypes.string,
+    bgImg: PropTypes.string,
+    textColor: PropTypes.string,
+    font: PropTypes.string,
+    video: PropTypes.string,
+};
+
+const FullHeader = ({ title, subtitle, bgColor, textColor, font, bgImg, video }) => {
     const headerStylesCombined = {
         ...headerSyle,
         backgroundColor: bgColor,
@@ -18,19 +35,11 @@ const FullHeader = ({ title, subtitle, bgColor, textColor, font, bgImg }) => {
                 {title && <h1 style={titleStyle}>{title}</h1>}
                 {subtitle && <h2 style={subTitleStyle}>{subtitle}</h2>}
             </div>
+            {video && <video autoPlay muted loop src={video} style={videoStyle}></video>}
         </header>
     );
 
     return component;
-};
-
-const propTypes = {
-    title: PropTypes.string,
-    subtitle: PropTypes.string,
-    bgColor: PropTypes.string,
-    bgImg: PropTypes.string,
-    textColor: PropTypes.string,
-    font: PropTypes.string,
 };
 
 FullHeader.propTypes = propTypes;
--- react-full-header/stories/Main.js
+++ react-full-header/stories/Main.js
@@ -4,51 +4,51 @@ import FullHeader from '../src/Main'; // This is our component
 import { storiesOf } from '@storybook/react';
 
 storiesOf('FullHeader', module)
-  .add('with title', () => (
-    <FullHeader title="TDD" />
-  ))
-  .add('with title and subtitle', () => (
-    <FullHeader
-      title="TDD"
-      subtitle="Curso de JS com TDD na prática" />
-  ))
-  .add('with title, subtitle, bgColor', () => (
-    <FullHeader
-      title="TDD"
-      subtitle="Curso de JS com TDD na prática"
-      bgColor="#3299BB"
-    />
-  ))
-  .add('with title, subtitle, bgColor, textColor', () => (
-    <FullHeader
-      title="TDD"
-      subtitle="Curso de JS com TDD na prática"
-      bgColor="#3299BB"
-      textColor="#FF9900"
-    />
-  ))
-  .add('with title, subtitle, bgColor, textColor, font', () => (
-    <FullHeader
-      title="TDD"
-      subtitle="Curso de JS com TDD na prática"
-      bgColor="#3299BB"
-      textColor="#FF9900"
-      font="cursive"
-    />
-  ))
-  .add('with title, subtitle, bgImage', () => (
-    <FullHeader
-      title="TDD"
-      subtitle="Curso de JS com TDD na prática"
-      bgImg="https://raw.githubusercontent.com/willianjusten/photo-examples/master/iceland-glacier.jpg"
-    />
-  ))
-  .add('with title, subtitle, video', () => (
-    <FullHeader
-      title="TDD"
-      subtitle="Curso de JS com TDD na prática"
-      bgColor="#3299BB"
-      textColor="#FF9900"
-      video="http://callmenick.com/_development/video/full-screen-background-video/media/demo.mp4"
-    />
-  ));
+    .add('with title', () => (
+        <FullHeader title="TDD" />
+    ))
+    .add('with title and subtitle', () => (
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JS com TDD na prática" />
+    ))
+    .add('with title, subtitle, bgColor', () => (
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JS com TDD na prática"
+            bgColor="#3299BB"
+        />
+    ))
+    .add('with title, subtitle, bgColor, textColor', () => (
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JS com TDD na prática"
+            bgColor="#3299BB"
+            textColor="#FF9900"
+        />
+    ))
+    .add('with title, subtitle, bgColor, textColor, font', () => (
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JS com TDD na prática"
+            bgColor="#3299BB"
+            textColor="#FF9900"
+            font="cursive"
+        />
+    ))
+    .add('with title, subtitle, bgImage', () => (
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JS com TDD na prática"
+            bgImg="https://raw.githubusercontent.com/willianjusten/photo-examples/master/iceland-glacier.jpg"
+        />
+    ))
+    .add('with title, subtitle, video', () => (
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JS com TDD na prática"
+            bgColor="#3299BB"
+            textColor="#FF9900"
+            video="http://callmenick.com/_development/video/full-screen-background-video/media/demo.mp4"
+        />
+    ));
