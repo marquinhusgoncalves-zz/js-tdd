d6aaec33b86beb7ac424357ad3563499f108b02e
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-03-11 23:41:46 -0300

    145 - Adicionando título

--- react-full-header/tests/specs/Main.spec.js
+++ react-full-header/tests/specs/Main.spec.js
@@ -5,8 +5,23 @@ import { shallow } from 'enzyme';
 import FullHeader from '../../src/Main';
 
 describe('<FullHeader />', () => {
-  it('should have header tag when mount', () => {
-    const wrapper = shallow(<FullHeader />);
-    expect(wrapper.find('header')).to.have.length(1);
-  });
+    it('should have header tag when mount', () => {
+        const wrapper = shallow(<FullHeader />);
+        expect(wrapper.find('header')).to.have.length(1);
+    });
+
+    it('should have h1 tag when mount', () => {
+        const wrapper = shallow(<FullHeader title="TDD" />);
+        expect(wrapper.find('h1')).to.have.length(1);
+    });
+
+    it('should not have h1 tag when mount', () => {
+        const wrapper = shallow(<FullHeader />);
+        expect(wrapper.find('h1')).to.have.length(0);
+    });
+
+    it('should have h1 tag with the title passed', () => {
+        const wrapper = shallow(<FullHeader title="TDD" />);
+        expect(wrapper.find('h1').props().children).to.be.equal('TDD');
+    });
 });
