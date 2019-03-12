526632ee95a2c072a744b9ee929b7cf9a2f55893
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-03-11 22:00:19 -0300

    138 - Adicionando um loader

--- btc-converter/bin/convertBTC.js
+++ btc-converter/bin/convertBTC.js
@@ -3,6 +3,12 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 var chalk = require('chalk');
 var request = require('request');
+var ora = require('ora');
+
+var spinner = ora({
+  text: 'Retrieving Bitcoin data...',
+  color: 'yellow'
+});
 
 function convertBTC() {
   var currency = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'USD';
@@ -10,8 +16,10 @@ function convertBTC() {
 
   var url = 'https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=' + currency + '&amount=' + amount;
 
+  spinner.start();
   request(url, function (error, response, body) {
     var apiResponse = void 0;
+    spinner.stop();
     try {
       apiResponse = JSON.parse(body);
     } catch (parseError) {
